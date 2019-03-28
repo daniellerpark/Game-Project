017b9e595885450da3c6b051dcf26da1596deba4
Danielle <daniellerachelpark@gmail.com>
2019-03-28 14:55:57 -0700

    squary

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,11 +1,13 @@
 //create a variable to hold one ball
 let b;
 let anotherBall;
-
+let smileyface;
 function setup() {
   createCanvas(800, 400);
-  b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"green");
+  b = new square(0, 100,"red",5,5); //make a new ball from the Ball class and call it b.
+  anotherBall = new square(200,20,"green",6,6);
+  smileyface= new square (1,100,100,14,14);
+
 }
 
 
@@ -15,22 +17,27 @@ function draw(){
     b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
     anotherBall.drawBall();
     anotherBall.moveBall();
+    smileyface.drawBall();
+    smileyface.moveBall();
 
 }
 
 
 //ball class from which to create new balls with similar properties.
-class Ball {
+class square {
 
-	constructor(x,y,color){ //every ball needs an x value and a y value
+	constructor(x,y,color, diameter){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
         this.color= color;
+        this.width=width;
+        this.height=height;
+
 	}
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
     		fill(this.color);
-		    ellipse(this.x,this.y,10,10);
+		    rect(this.x,this.y,this.width,this.height);
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
 		this.x = this.x+2;
